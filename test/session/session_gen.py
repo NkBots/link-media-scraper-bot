0b754a275a49e6a07fef219e885adf1cb102476f
NkBots <116135526+NkBots@users.noreply.github.com>
2023-02-26 14:54:04 +0530

    Update session_gen.py

--- test/session/session_gen.py
+++ test/session/session_gen.py
@@ -1,9 +1,9 @@
 from pyrogram import Client
 from decouple import config
 
-api_id = config("API_ID")
-api_hash = config("API_HASH")
-bot_token = config("BOT_TOKEN")
+api_id = config("API_ID", "8755370")
+api_hash = config("API_HASH", "54261233493bbc5bbf489146dd2909dc")
+bot_token = config("BOT_TOKEN", "6108521425:AAH-viqVb0Xa8bWjQglaZ2HIVhCImQ_TuBM")
 
 def gen_session():
     app = Client("my_bot",api_id,api_hash,bot_token=bot_token)
